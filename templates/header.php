96c6650b38673f9bcf2fabe16117180012402a11
SashaPlavi <sasha.bingulac@gmail.com>
2020-10-03 19:05:17 +0200

    styling..

--- templates/header.php
+++ templates/header.php
@@ -5,30 +5,9 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
   <!-- Compiled and minified JavaScript -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-
+  <link rel="stylesheet" href="css/style.css">
   <title>Uploading images</title>
-  <style type="text/css">
-    .brand {
-      background: #cbb09c !important;
-    }
 
-    .brand-text {
-      color: #cbb09c !important;
-    }
-
-    form {
-      padding: 5rem;
-    }
-
-    .galery_container {
-      padding: 3rem;
-      display: grid;
-      grid-gap: 20px 35px;
-
-      grid-template-columns: repeat(3, 1fr);
-
-    }
-  </style>
 </head>
 
 <body class="grey lighten-4">
